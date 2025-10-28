fef369b419b51137742c7b406a0988a4a2ad0360
sarwm-at-tu-chemnitz.de <sarwar.morshad@s2020.tu-chemnitz.de>
2025-10-28 11:39:05 +0100

    Profile Edit Update

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -8,6 +8,7 @@ const Profile = () => {
       <button>Delete Account</button>
       <button>View Activity Log</button>
       <button>Change Password</button>
+      <button>Logout</button>
     </div>
   );
 };
